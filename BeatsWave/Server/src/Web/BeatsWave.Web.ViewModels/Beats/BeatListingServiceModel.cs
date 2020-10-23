b5ab2ab9974d584fc423e604fd2203ccdf504164
Georgi Popov <popovgeorgi002@gmail.com>
2020-10-23 19:15:12 +0300

    BeatUrl has been renamed to Url on the API

--- BeatsWave/Server/src/Web/BeatsWave.Web.ViewModels/Beats/BeatListingServiceModel.cs
+++ BeatsWave/Server/src/Web/BeatsWave.Web.ViewModels/Beats/BeatListingServiceModel.cs
@@ -1,18 +1,26 @@
 ﻿namespace BeatsWave.Web.Models.Beats
 {
+    using AutoMapper;
     using BeatsWave.Data.Models;
     using BeatsWave.Services.Mapping;
 
-    public class BeatListingServiceModel : IMapFrom<Beat>
+    public class BeatListingServiceModel : IMapFrom<Beat>, IHaveCustomMappings
     {
         public int Id { get; set; }
+
+        public string Url { get; set; }
 
         public string Name { get; set; }
 
         public string ImageUrl { get; set; }
 
-        public string BeatUrl { get; set; }
-
         public string ProducerUserName { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration
+                .CreateMap<Beat, BeatListingServiceModel>()
+                .ForMember(b => b.Url, m => m.MapFrom(b => b.BeatUrl));
+        }
     }
 }
