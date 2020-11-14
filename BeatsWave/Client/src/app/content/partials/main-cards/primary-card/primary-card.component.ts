b87f057f7afbedaf100d5a815a86396ee12c7fa5
Georgi Popov <popovgeorgi002@gmail.com>
2020-11-14 12:52:41 +0200

    Beats are likable on the feed

--- BeatsWave/Client/src/app/content/partials/main-cards/primary-card/primary-card.component.ts
+++ BeatsWave/Client/src/app/content/partials/main-cards/primary-card/primary-card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { AudioPlayerService } from '../../../../core/services/audio-player.service';
 import { Beat } from 'src/app/core/models/Beat';
+import { LikeService } from 'src/app/core/services/like.service';
+import { SnotifyService } from 'ng-snotify';
 
 @Component({
     selector: 'app-primary-card',
@@ -15,21 +17,29 @@ export class PrimaryCardComponent implements OnInit {
 
     classes = '';
 
-    constructor(private audioPlayerService: AudioPlayerService) {
+    constructor(private audioPlayerService: AudioPlayerService,
+      private likeService: LikeService,
+      private snotifyService: SnotifyService) {
     }
 
     ngOnInit() {
         this.classes = 'custom-card--img ' + this.imageBorderRadiusClass;
     }
 
-    // addFavorite() {
-    //     this.song.favorite = true;
-    // }
+    addFavorite() {
+      this.likeService.vote(this.song.id).subscribe(res => {
+        if (res == true) {
+          this.snotifyService.success('Liked ' + this.song.name);
+        }
+        else {
+          this.snotifyService.success('Unliked ' + this.song.name);
+        }
+      });
+    }
 
     getSongInfo() {}
 
     addInPlayer() {
-      debugger;
         this.audioPlayerService.playSong(this.song);
     }
 
--- BeatsWave/Client/src/app/content/partials/song-options/song-options.component.ts
+++ BeatsWave/Client/src/app/content/partials/song-options/song-options.component.ts
@@ -1,4 +1,7 @@
 import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { SnotifyService } from 'ng-snotify';
+import { Beat } from 'src/app/core/models/Beat';
+import { LikeService } from 'src/app/core/services/like.service';
 
 @Component({
     selector: 'app-song-options',
@@ -8,23 +11,30 @@ export class SongOptionsComponent implements OnInit {
 
     @HostBinding('class') class = 'dropleft';
 
-    @Input() song: any;
+    @Input() song: Beat;
     @Input() icon = '';
 
-    constructor() { }
+    private isLiked: boolean = false;
+
+    constructor(private likeService: LikeService,
+      private snotifyService: SnotifyService) { }
 
     ngOnInit() {
         this.icon = 'la ' + this.icon;
     }
 
     addFavorite() {
-        this.song.favorite = true;
+        this.likeService.vote(this.song.id).subscribe(res => {
+          if (res == true) {
+            this.snotifyService.success('Liked ' + this.song.name);
+          }
+          else {
+            this.snotifyService.success('Unliked ' + this.song.name);
+          }
+        });
     }
 
     addToPlayList() {
-    }
-
-    shareSong() {
     }
 
 }
