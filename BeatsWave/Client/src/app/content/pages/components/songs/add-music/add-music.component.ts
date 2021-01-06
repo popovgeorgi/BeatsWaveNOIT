921cf7d44f141b32455e1af88d7a99ae07c5e516
Georgi Popov <popovgeorgi002@gmail.com>
2021-01-06 21:53:42 +0200

    Better UI for uploading a beat

--- BeatsWave/Client/src/app/content/pages/components/songs/add-music/add-music.component.ts
+++ BeatsWave/Client/src/app/content/pages/components/songs/add-music/add-music.component.ts
@@ -1,66 +1,73 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 
-import { LoadingService } from '../../../../../core/services/loading.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { BeatService } from 'src/app/core/services/beat.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
-    selector: 'app-add-music',
-    templateUrl: './add-music.component.html'
+  selector: 'app-add-music',
+  templateUrl: './add-music.component.html'
 })
-export class AddMusicComponent implements OnInit, AfterViewInit {
+export class AddMusicComponent implements AfterViewInit {
 
-    public beatForm: FormGroup;
-    public uploadSaveImageUrl: string = environment.apiUrl + '/FileUpload/SavePhoto';
-    public uploadSaveBeatUrl: string = environment.apiUrl + '/FileUpload/SaveBeat';
+  public beatForm: FormGroup;
+  public uploadSaveImageUrl: string = environment.apiUrl + '/FileUpload/SavePhoto';
+  public uploadSaveBeatUrl: string = environment.apiUrl + '/FileUpload/SaveBeat';
 
 
-    constructor(private spinner: NgxSpinnerService,
-        private fb: FormBuilder,
-        private beatService: BeatService) {
-        this.beatForm = this.fb.group({
-            "name": ['', [Validators.required]],
-            "beatUrl": ['', [Validators.required]],
-            "imageUrl": ['', [Validators.required]],
-            "price": ['', [Validators.required]],
-            "genre": ['', [Validators.required]],
-            "bpm": [''],
-            "description": ['']
-        })
-    }
+  constructor(private spinner: NgxSpinnerService,
+    private fb: FormBuilder,
+    private beatService: BeatService) {
+    this.beatForm = this.fb.group({
+      "name": ['', [Validators.required]],
+      "beatUrl": ['', [Validators.required]],
+      "imageUrl": ['', [Validators.required]],
+      "price": ['', [Validators.required]],
+      "genre": ['', [Validators.required]],
+      "bpm": [''],
+      "description": ['']
+    })
+  }
 
-    public onPhotoUploading() {
-        this.spinner.show('beatUploader');
-    }
+  public onPhotoUploading() {
+    this.spinner.show('beatUploader');
+  }
 
-    public onPhotoUploaded(e) {
-        this.beatForm.controls['imageUrl'].setValue(e.originalEvent.body.uri)
-        this.spinner.hide('beatUploader');
-    }
+  public onPhotoUploaded(e) {
+    this.beatForm.controls['imageUrl'].setValue(e.originalEvent.body.uri)
+    this.spinner.hide('beatUploader');
+  }
 
-    public onBeatUploading() {
-        this.spinner.show('beatUploader');
-    }
+  public onBeatUploading() {
+    this.spinner.show('beatUploader');
+  }
 
-    public onBeatUploaded(e) {
-        this.beatForm.controls['beatUrl'].setValue(e.originalEvent.body.uri)
-        this.spinner.hide('beatUploader');
-    }
+  public onBeatUploaded(e) {
+    this.beatForm.controls['beatUrl'].setValue(e.originalEvent.body.uri)
+    this.spinner.hide('beatUploader');
+  }
 
-    public uploadBeat() {
-        this.spinner.show('beatUploader');
-        this.beatService.uploadBeat(this.beatForm.value).subscribe(res => {
-            this.spinner.hide('beatUploader');
-        })
-    }
+  public uploadBeat() {
+    this.spinner.show('beatUploader');
+    this.beatService.uploadBeat(this.beatForm.value).subscribe(res => {
+      this.spinner.hide('beatUploader');
+    })
+  }
 
-    ngOnInit() {
-    }
+  get name() {
+    return this.beatForm.get('name');
+  }
 
-    ngAfterViewInit() {
-        this.spinner.hide('routing');
-    }
+  get price() {
+    return this.beatForm.get('price');
+  }
 
+  get genre() {
+    return this.beatForm.get('genre');
+  }
+
+  ngAfterViewInit() {
+    this.spinner.hide('routing');
+  }
 }
