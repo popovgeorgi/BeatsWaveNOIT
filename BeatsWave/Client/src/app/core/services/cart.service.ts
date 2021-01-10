4724fa5c1afa5e5d93e5919ce98af05d056ac815
Georgi Popov <popovgeorgi002@gmail.com>
2021-01-10 13:25:04 +0200

    Importing css class to the photo of the comment

--- BeatsWave/Client/src/app/core/services/cart.service.ts
+++ BeatsWave/Client/src/app/core/services/cart.service.ts
@@ -13,6 +13,9 @@ export class CartService {
   constructor(private localStorageService: LocalStorageService) { }
 
   add(id: number) {
+    if (this.localStorageService.getLocalStorage(this.cartName) == null) {
+      this.localStorageService.setLocalStorage(this.cartName, Array<number>());
+    }
     let cart: Array<number> = this.localStorageService.getLocalStorage(this.cartName);
     if (cart.includes(id)) {
       return 'You already have that beat in your cart!';
