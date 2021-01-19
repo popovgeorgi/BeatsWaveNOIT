d15041edd81574add7a458911203d1d019ceb9cf
Georgi Popov <popovgeorgi002@gmail.com>
2021-01-19 21:21:28 +0200

    Frond-end validation on the event form

--- BeatsWave/Client/src/app/content/pages/components/events/add-event/add-event.component.ts
+++ BeatsWave/Client/src/app/content/pages/components/events/add-event/add-event.component.ts
@@ -1,16 +1,14 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { EventService } from 'src/app/core/services/event.service';
 import { environment } from 'src/environments/environment';
-
-import { LoadingService } from '../../../../../core/services/loading.service';
 
 @Component({
   selector: 'app-add-event',
   templateUrl: './add-event.component.html'
 })
-export class AddEventComponent implements OnInit, AfterViewInit {
+export class AddEventComponent implements AfterViewInit {
 
   public eventForm: FormGroup;
   public uploadSaveImageUrl: string = environment.apiUrl + '/FileUpload/SavePhoto';
@@ -18,19 +16,40 @@ export class AddEventComponent implements OnInit, AfterViewInit {
   constructor(private spinner: NgxSpinnerService,
     private fb: FormBuilder,
     private eventService: EventService) {
-      this.eventForm = this.fb.group({
-        "name": ['', [Validators.required]],
-        "imageUrl": ['', [Validators.required]],
-        "venue": ['', [Validators.required]],
-        "phoneNumber": ['', [Validators.required]],
-        "email": ['', [Validators.required, Validators.email]],
-        "conductDate": ['', [Validators.required]],
-        "description": [''],
-        "price": ['']
-      })
+    this.eventForm = this.fb.group({
+      "name": ['', [Validators.required]],
+      "imageUrl": ['', [Validators.required]],
+      "venue": ['', [Validators.required]],
+      "phoneNumber": ['', [Validators.required]],
+      "email": ['', [Validators.required, Validators.email]],
+      "conductDate": ['', [Validators.required, Validators.pattern(/^\d{4}-[01]{1}[\d]{1}-[0123]{1}[\d]$/)]],
+      "description": ['', [Validators.maxLength(500)]],
+      "price": ['']
+    })
   }
 
-  ngOnInit() {
+  get name() {
+    return this.eventForm.get('name');
+  }
+
+  get venue() {
+    return this.eventForm.get('venue');
+  }
+
+  get phoneNumber() {
+    return this.eventForm.get('phoneNumber');
+  }
+
+  get email() {
+    return this.eventForm.get('email');
+  }
+
+  get conductDate() {
+    return this.eventForm.get('conductDate');
+  }
+
+  get description() {
+    return this.eventForm.get('description');
   }
 
   public onPhotoUploading() {
