b95ce7921245ba14da4a8fbf70fc4890881d5682
Georgi Popov <popovgeorgi002@gmail.com>
2021-02-08 21:16:21 +0200

    Better spinner experience

--- BeatsWave/Client/src/app/content/pages/components/songs/song-details/song-details.component.ts
+++ BeatsWave/Client/src/app/content/pages/components/songs/song-details/song-details.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Observable, Subscription } from "rxjs";
 
