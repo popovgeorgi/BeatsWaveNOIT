e57f334da51ab29278e992fe0290870ae6a90f65
Georgi Popov <popovgeorgi002@gmail.com>
2021-02-08 18:36:16 +0200

    BeatsByGenre is now optimal

--- BeatsWave/Client/src/app/content/pages/components/genres/genre-listing/genre-listing.component.ts
+++ BeatsWave/Client/src/app/content/pages/components/genres/genre-listing/genre-listing.component.ts
@@ -1,9 +1,11 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { map, mergeMap } from 'rxjs/operators';
+import { forkJoin, Observable } from 'rxjs';
+import { map, mergeMap, tap } from 'rxjs/operators';
 import { Beat } from 'src/app/core/models/Beat';
 import { BeatService } from 'src/app/core/services/beat.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-genre-listing',
@@ -18,17 +20,29 @@ export class GenreListingComponent implements OnInit, AfterViewInit {
   private itemsPerPage: number = 20;
   private page = 1;
   private genre: string;
+  private userFavourites: Array<number>;
 
   constructor(private beatService: BeatService,
     private route: ActivatedRoute,
-    private spinner: NgxSpinnerService) { }
+    private spinner: NgxSpinnerService,
+    private userService: UserService) { }
 
   ngOnInit() {
-    this.fetchInitialBeats();
+    this.fetchInitialBeats().subscribe(results => {
+      this.userFavourites = results[1];
+      this.setFeedLikes(results[0], this.userFavourites);
+
+      let beats = results[0];
+      if (beats.length < this.itemsPerPage) {
+        this.hasMoreBeatsToInclude = false;
+      }
+      this.beats = beats;
+      this.beatsCount = beats.length;
+    });
   }
 
-  private fetchInitialBeats() {
-    this.route.params.pipe(map(params => {
+  private fetchInitialBeats(): Observable<[Beat[], number[]]> {
+    return this.route.params.pipe(map(params => {
       let genre = params['name'];
       if (genre == 'Hip-Hop') {
         genre = 'HipHop';
@@ -39,22 +53,55 @@ export class GenreListingComponent implements OnInit, AfterViewInit {
       this.genre = genre;
       return genre;
     }),
-      mergeMap(genre => this.beatService.getBeatsByGenre(genre, this.itemsPerPage, (this.page - 1) * this.itemsPerPage))).subscribe(beats => {
-        if (beats.length < this.itemsPerPage) {
-          this.hasMoreBeatsToInclude = false;
-        }
-        this.beats = beats;
-        this.beatsCount = beats.length;
-      })
+      mergeMap(genre => forkJoin([this.fetchGenreBeats(genre), this.fetchUserFavourites()])));
+  }
+
+  private fetchGenreBeats(genre: string): Observable<Array<Beat>> {
+    return this.beatService.getBeatsByGenre(genre, this.itemsPerPage, (this.page - 1) * this.itemsPerPage)
   }
 
   showMore() {
     this.page++;
-    this.beatService.getBeatsByGenre(this.genre, this.itemsPerPage, (this.page - 1) * this.itemsPerPage).subscribe(beats => {
+    this.beatService.getBeatsByGenre(this.genre, this.itemsPerPage, (this.page - 1) * this.itemsPerPage)
+    .pipe(tap((res) => {
+      if (this.userFavourites) {
+        this.setFeedLikes(res, this.userFavourites);
+      }
+    }))
+    .subscribe(beats => {
       if (beats.length < this.itemsPerPage) {
         this.hasMoreBeatsToInclude = false;
       }
       this.beats = this.beats.concat(beats);
+    })
+  }
+
+  public onSelect(event) {
+    let option = event.target.value;
+    if (option == 0) {
+      this.beats = this.beats.sort((a, b) => {
+        return <any>new Date(b.createdOn) - <any>new Date(a.createdOn);
+      });
+    }
+    else if (option == 1) {
+      this.beats = this.beats.sort((a, b) => b.likesCount - a.likesCount);
+    }
+  }
+
+  private fetchUserFavourites(): Observable<Array<number>> {
+    return this.userService.getFavouritesByIds();
+  }
+
+  private setFeedLikes(beats: Beat[], userFavourites: Array<number>) {
+    let map = new Map<string, Array<number>>();
+    map.set('favourites', userFavourites);
+    beats.forEach(beat => {
+      if (map.get('favourites').includes(beat.id)) {
+        beat.isLiked = true;
+      }
+      else {
+        beat.isLiked = false;
+      }
     })
   }
 
