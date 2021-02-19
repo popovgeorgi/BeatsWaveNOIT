715c6e888157497830a0d2d023aca15f0cb5cca8
Georgi Popov <popovgeorgi002@gmail.com>
2021-02-19 12:07:31 +0200

    Tests written for Beats and Search services

--- BeatsWave/Server/src/Tests/BeatsWave.Services.Data.Tests/BeatServiceTests.cs
+++ BeatsWave/Server/src/Tests/BeatsWave.Services.Data.Tests/BeatServiceTests.cs
@@ -6,10 +6,12 @@
     using System.Threading.Tasks;
 
     using BeatsWave.Data;
+    using BeatsWave.Data.Common.Repositories;
     using BeatsWave.Data.Models;
     using BeatsWave.Data.Repositories;
     using BeatsWave.Services.Mapping;
     using Microsoft.EntityFrameworkCore;
+    using Moq;
     using Xunit;
 
     public class BeatServiceTests
@@ -23,9 +25,8 @@
         public async Task CheckIfAllMethodReturnsTheCorrectAmountOfData(int? take = null, int skip = 0)
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
-               .UseInMemoryDatabase("BeatsWaveVirtual");
+               .UseInMemoryDatabase(Guid.NewGuid().ToString());
             var dbContext = new ApplicationDbContext(optionsBuilder.Options);
-            dbContext.Database.EnsureDeleted();
             var beatRepo = new EfDeletableEntityRepository<Beat>(dbContext);
             var playRepo = new EfRepository<Play>(dbContext);
             var beatService = new BeatService(beatRepo, playRepo);
@@ -43,10 +44,69 @@
             await beatRepo.SaveChangesAsync();
             AutoMapperConfig.RegisterMappings(typeof(FakeBeatModel).Assembly);
             var result = await beatService.AllAsync<FakeBeatModel>(take, skip);
+            dbContext.Database.EnsureDeleted();
             result.OrderBy(x => x.Id);
 
             Assert.Equal(take, result.ToList().Count);
             Assert.Equal(skip + 1, result.First().Id);
+        }
+
+        [Fact]
+        public void CheckIfAddPlayMethodDoesNotAddPlayToTheSameUserThatRequested()
+        {
+            var plays = new List<Play>();
+            var beats = new List<Beat>();
+            var beatRepo = new Mock<IDeletableEntityRepository<Beat>>();
+            beatRepo.Setup(b => b.AddAsync(It.IsAny<Beat>())).Callback(
+                (Beat beat) => beats.Add(beat));
+            beatRepo.Setup(b => b.All()).Returns(beats.AsQueryable());
+            var playRepo = new Mock<IRepository<Play>>();
+            playRepo.Setup(p => p.AddAsync(It.IsAny<Play>())).Callback(
+                (Play play) => plays.Add(play));
+            playRepo.Setup(r => r.All()).Returns(plays.AsQueryable());
+            var beatService = new BeatService(beatRepo.Object, playRepo.Object);
+
+            beatService.CreateAsync("test", "test", "test", 10, Genre.Rock.ToString(), 112, "test", "user").GetAwaiter();
+            beatService.AddPlay(0, "user").GetAwaiter();
+
+            Assert.Empty(plays);
+        }
+
+        [Fact]
+        public void CheckIfUserCannotEditOtherUserBeat()
+        {
+            var plays = new List<Play>();
+            var beats = new List<Beat>();
+            var beatRepo = new Mock<IDeletableEntityRepository<Beat>>();
+            beatRepo.Setup(b => b.AddAsync(It.IsAny<Beat>())).Callback(
+                (Beat beat) => beats.Add(beat));
+            beatRepo.Setup(b => b.All()).Returns(beats.AsQueryable());
+            var playRepo = new Mock<IRepository<Play>>();
+            var beatService = new BeatService(beatRepo.Object, playRepo.Object);
+
+            beatService.CreateAsync("test", "test", "test", 10, Genre.Rock.ToString(), 112, "test", "user").GetAwaiter();
+            beatService.UpdateAsync("anotherUser", 0, "hack", 100, Genre.HipHop.ToString(), 120, "hack").GetAwaiter();
+
+            Assert.Equal("test", beats.First().Name);
+        }
+
+        [Fact]
+
+        public void CheckIfUserCannotDeleteOtherUserBeat()
+        {
+            var plays = new List<Play>();
+            var beats = new List<Beat>();
+            var beatRepo = new Mock<IDeletableEntityRepository<Beat>>();
+            beatRepo.Setup(b => b.AddAsync(It.IsAny<Beat>())).Callback(
+                (Beat beat) => beats.Add(beat));
+            beatRepo.Setup(b => b.All()).Returns(beats.AsQueryable());
+            var playRepo = new Mock<IRepository<Play>>();
+            var beatService = new BeatService(beatRepo.Object, playRepo.Object);
+
+            beatService.CreateAsync("test", "test", "test", 10, Genre.Rock.ToString(), 112, "test", "user").GetAwaiter();
+            beatService.DeleteAsync("hacker", 0).GetAwaiter();
+
+            Assert.Single(beats);
         }
 
         public class FakeBeatModel : IMapFrom<Beat>
